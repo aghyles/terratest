b6d17f52b55086c4be36692235173357bf3f15f5
aghyles <aghyles@gmail.com>
2023-10-04 19:01:43 +0100

    test 10

--- src/app/api/audites/route.ts
+++ src/app/api/audites/route.ts
@@ -2,10 +2,11 @@ export async function GET() {
   const res = await fetch('http://127.0.0.1:1337/api/audits?populate=*', {
     headers: {
       'Content-Type': 'application/json',
-      'API-Key': process.env.DATA_API_KEY,
+      'API-Key': process.env.DATA_API_KEY || '',
     },
-  })
-  const data = await res.json()
+  });
 
-  return Response.json({ data })
+  const data = await res.json();
+
+  return Response.json({ data });
 }
