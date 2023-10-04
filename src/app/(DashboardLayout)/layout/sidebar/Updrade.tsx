92373bc40a3419707672840a1995b82d461e3cf0
aghyles <aghyles@gmail.com>
2023-10-04 18:47:11 +0100

    test 10

--- src/app/(DashboardLayout)/layout/sidebar/Updrade.tsx
+++ src/app/(DashboardLayout)/layout/sidebar/Updrade.tsx
@@ -1,8 +1,8 @@
 import { Box, Typography, Button } from '@mui/material';
-import img1 from 'public/images/backgrounds/rocket.png';
 import Image from 'next/image';
 import Link from 'next/link';
 
+const img1 = '/images/backgrounds/rocket.png';
 export const Upgrade = () => {
     return (
         <Box
