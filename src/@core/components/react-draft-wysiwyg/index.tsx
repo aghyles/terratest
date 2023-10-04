578e730a80370d25a1642237ec0852aa0630a4a5
aghyles <aghyles@gmail.com>
2023-10-04 21:14:14 +0100

    test 10

---
+++ src/@core/components/react-draft-wysiwyg/index.tsx
@@ -0,0 +1,19 @@
+// ** Types
+interface ReactDraftWysiwygProps {
+
+  // Propriétés du composant
+}
+
+const ReactDraftWysiwyg: React.FC<ReactDraftWysiwygProps> = ({ /* Propriétés ici */ }) => {
+  // Contenu du composant
+
+  return (
+
+    // JSX du composant
+    <div>
+      {/* Contenu JSX ici */}
+    </div>
+  );
+};
+
+export default ReactDraftWysiwyg;
